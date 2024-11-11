c2b3b87793425d490e57f6f9860fca9326dfa968
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2024-11-11 20:24:06 +0000

    feat: calculates monthly std dev for each feature

---
+++ sql/02_monthly_standard_deviations.sql
@@ -0,0 +1,19 @@
+-- Creates a new table to store the monthly standard deviations of each variable --
+CREATE TABLE monthly_std_dev AS
+WITH std_dev AS (
+    SELECT
+        EXTRACT(YEAR FROM date) AS year,
+        EXTRACT(MONTH FROM date) AS month,
+        STDDEV(cloud_cover) AS stddev_cloud_cover,
+        STDDEV(sunshine) AS stddev_sunshine,
+        STDDEV(global_radiation) AS stddev_global_radiation,
+        STDDEV(max_temp) AS stddev_max_temp,
+        STDDEV(mean_temp) AS stddev_mean_temp,
+        STDDEV(min_temp) AS stddev_min_temp,
+        STDDEV(precipitation) AS stddev_precipitation,
+        STDDEV(pressure) AS stddev_pressure,
+        STDDEV(snow_depth) AS stddev_snow_depth
+    FROM readings
+    GROUP BY EXTRACT(YEAR FROM date), EXTRACT(MONTH FROM date)
+)
+SELECT * FROM std_dev;

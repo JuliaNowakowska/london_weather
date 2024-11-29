7ee1891394517490d444c4dd14d2e44dbb41027a
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2024-11-29 20:35:04 +0000

    feat: added the line chart

--- scripts/main.py
+++ scripts/main.py
@@ -11,20 +11,26 @@ if __name__ == "__main__":
     }
 
     # Path to your SQL file
-    sql_file_path = 'sql/07_max_temp.sql'
+    sql_file_path = '/sql/07_max_temp.sql'
 
     # Execute the SQL script and fetch results
     results = get_data(pg_config, sql_file_path)
-    max_temp_date = results[0][0]
-    max_temp = results[0][1]
+    max_temp_date = results[1][0]
+    max_temp = results[1][1]
 
     # Path to your SQL file
-    sql_file_path = 'sql/06_min_temp.sql'
+    sql_file_path = '/sql/06_min_temp.sql'
 
     # Execute the SQL script and fetch results
     results = get_data(pg_config, sql_file_path)
     min_temp_date = results[0][0]
     min_temp = results[0][1]
 
-    app = create_dashboard(max_temp_date, max_temp, min_temp_date, min_temp)
+    sql_file_path = '/sql/08_get_monthly_avg.sql'
+
+    # Execute the SQL script and fetch results
+    results_monthly_avg = get_data(pg_config, sql_file_path)
+    monthly_avg_temp = convert_to_dataframe(results_monthly_avg)
+
+    app = create_dashboard(max_temp_date, max_temp, min_temp_date, min_temp, monthly_avg_temp)
     app.run_server(debug=True)

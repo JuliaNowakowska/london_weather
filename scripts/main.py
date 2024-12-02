f94b5c6088831b9a897e63e4f04e703ce7a4d779
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2024-12-02 19:13:57 +0000

    refactor: optimized query calling

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
+import os
 from config import PG_CONFIG
-from database_handler import Database
+from data_pipeline import Database
 from weather_dashboard import create_dashboard
 from monthly_dataframe import convert_to_dataframe
 
@@ -7,31 +8,21 @@ if __name__ == "__main__":
     # Establishing a connection with the database
     db = Database(PG_CONFIG)
 
-    # Placeholder for the results
-    results = []
+    # Identifying SQL scripts
+    current_dir = os.path.dirname(__file__)
+    max_temp_sql = os.path.join(current_dir, '../sql/07_max_temp.sql')
+    min_temp_sql = os.path.join(current_dir, '../sql/06_min_temp.sql')
+    monthly_avg_sql = os.path.join(current_dir, '../sql/08_get_monthly_avg.sql')
 
+    # Querying the database
     if db.connection:
         try:
-            sql_scripts = ['/sql/07_max_temp.sql',
-                           '/sql/06_min_temp.sql',
-                           '/sql/08_get_monthly_avg.sql']
-
-            for sql_script in sql_scripts:
-                sql_query = db.read_sql(sql_script)
-                results.append(db.query(sql_query))
+            max_temp_date, max_temp = db.query(max_temp_sql)[0][0], db.query(max_temp_sql)[0][1]
+            min_temp_date, min_temp = db.query(min_temp_sql)[0][0], db.query(min_temp_sql)[0][1]
+            monthly_avg_temp = convert_to_dataframe(db.query(monthly_avg_sql))
         finally:
             db.connection.close()
 
-    # Max temp
-    max_temp_date = results[0][0][0]
-    max_temp = results[0][0][1]
-
-    # Min temp
-    min_temp_date = results[1][0][0]
-    min_temp = results[1][0][1]
-
-    # Monthly averages
-    monthly_avg_temp = convert_to_dataframe(results[2])
-
+    # Running the dashboard
     app = create_dashboard(max_temp_date, max_temp, min_temp_date, min_temp, monthly_avg_temp)
     app.run_server(debug=True)

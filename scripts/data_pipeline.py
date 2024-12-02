2ee0edb07738f46f6caefd4f3e4758952df76c96
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2024-12-02 14:49:07 +0000

    feat: data connection separated

--- scripts/data_pipeline.py
+++ scripts/data_pipeline.py
@@ -1,12 +1,10 @@
 import psycopg2
 
-def get_data(pg_config, sql_file_path):
+def db_connection(pg_config):
+    """
+    Establishes a connection with the database and returns the connection object if successful.
+    """
     try:
-        # Read the SQL query from the file
-        with open(sql_file_path, 'r') as file:
-            sql_query = file.read()
-
-        # Connect to the PostgreSQL database
         connection = psycopg2.connect(
             host=pg_config['host'],
             port=pg_config['port'],
@@ -14,18 +12,8 @@ def get_data(pg_config, sql_file_path):
             user=pg_config['user'],
             password=pg_config['password']
         )
-        cursor = connection.cursor()
+        return connection
 
-        # Execute the SQL query
-        cursor.execute(sql_query)
-        result = cursor.fetchall()  # Fetch all rows from the query
-
-        return result
-    except Exception as e:
-        print(f"Error: {e}")
+    except psycopg2.DatabaseError as db_error:
+        print(f"Database connection error: {db_error}")
         return None
-    finally:
-        # Ensure the connection is closed
-        if connection:
-            cursor.close()
-            connection.close()

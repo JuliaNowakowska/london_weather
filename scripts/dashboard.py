10df50ebf867c6fd761bcf18d9f3f6c83cb7fe78
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2024-12-24 21:52:52 +0100

    feat: temp prediction card

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -5,16 +5,29 @@ import plotly.express as px
 
 
 class WeatherDashboard:
-    def __init__(self, max_temp_date, max_temp, min_temp_date, min_temp, temperature_data):
+    def __init__(self, max_temp_date, max_temp, min_temp_date, min_temp, temperature_data, predicted_temp):
         self.max_temp_date = max_temp_date
         self.max_temp = max_temp
         self.min_temp_date = min_temp_date
         self.min_temp = min_temp
         self.temperature_data = temperature_data
+        self.predicted_temp = predicted_temp
 
         # Initialize the Dash app
         self.app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+
+    def predicted_temp_card(self):
+        return dbc.Card(
+            [
+                dbc.CardHeader("Predicted Temperature"),
+                dbc.CardBody(
+                    html.H3(f"{self.predicted_temp}°C", className="card-title text-success"),
+                ),
+            ],
+            className="shadow-sm",
+        )
+    
     def temperature_card(self, title, temperature, date):
         return dbc.Card(
             [
